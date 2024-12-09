957f25eadce629e8b37fa54a527de44b1e20b529
Tapasya Pateriya <tapasyapateriya04@gmail.com>
2024-12-09 23:27:28 +0530

    Update Header.jsx

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,18 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { Button } from '../ui/button'
-import { Link } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
 
 function Header() {
   return (
     <div className='p-3 px-5 flex justify-between shadow-md'>
 
     <img src='/logo.svg' width={50} height={50}/>
-
-
-    <Link to={'/au'}
-    <Button>Get Started</Button>
-     
+    <Link to={'/dashboard'}>
+   <Button >Dashboard</Button>
+   </Link>
+  
     </div>
   )
 }
