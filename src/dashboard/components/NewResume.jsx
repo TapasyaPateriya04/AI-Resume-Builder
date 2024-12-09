e1a62c51bd562fc149dc54d0fb4456e24e252cfa
Tapasya Pateriya <tapasyapateriya04@gmail.com>
2024-12-09 23:27:35 +0530

    Create NewResume.jsx

---
+++ src/dashboard/components/NewResume.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { PlusSquare } from 'lucide-react'
+import React from 'react'
+
+function NewResume() {
+  return (
+    <div>
+      <div className='p-14 py-24 border items-center 
+      flex justify-center bg-secondary rounded-lg
+       h-[280px] hover:scale-105 tran'>
+        <PlusSquare/>
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default NewResume
